e88fbd9a39d7d3044f3b16f6a7c8208f95144ba6
re_magr0003 <martin.grotz@mathema.de>
2021-09-29 16:33:49 +0200

    delete code, so we can start with a clean slate again

--- math.js
+++ math.js
@@ -1,8 +1,4 @@
 function signum(num) {
-    if (isInvalid(num)) {
-        return undefined;
-    }
-
     if (num > 0) {
         return 1;
     } else if (num < 0) {
@@ -14,16 +10,6 @@ function signum(num) {
 
 function sum(nums) {
     return nums.reduce((a, b) => a + b, 0);
-}
-
-function isInvalid(num) {
-    return (
-        num === undefined ||
-        num === false ||
-        num === '' ||
-        isNaN(num) ||
-        num === null
-    );
 }
 
 module.exports = {
