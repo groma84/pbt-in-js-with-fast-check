8872404554bc6003314d4a191d43a3a79e72d8b6
re_magr0003 <martin.grotz@mathema.de>
2021-09-30 08:11:11 +0200

    make everything cleared

--- math.js
+++ math.js
@@ -1,15 +1,9 @@
 function signum(num) {
-    if (num > 0) {
-        return 1;
-    } else if (num < 0) {
-        return -1;
-    } else {
-        return 0;
-    }
+    return 0;
 }
 
 function sum(nums) {
-    return nums.reduce((a, b) => a + b, 0);
+    return 0;
 }
 
 module.exports = {
--- math.spec.js
+++ math.spec.js
@@ -2,21 +2,9 @@ const math = require('./math.js');
 const fc = require('fast-check');
 
 describe('signum', () => {
-    describe('example-based tests', () => {
 
-    });
-
-    describe('property-based tests', () => {
-
-    });
 });
 
 describe('sum', () => {
-    describe('example-based', () => {
 
-    });
-
-    describe('property-based', () => {
-
-    });
 });
